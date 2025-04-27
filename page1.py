59743cc159fcba696c02d921c3dae2869474cc3f
Sangnhan <105200425@sv1.dut.udn.vn>
2025-04-27 14:57:28 +0700

    Add files via upload

---
+++ page1.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(page_title="page1", page_icon="🏠")
+
+st.title("🏠 Ứng dụng Dự báo chỉ số công tơ điện")
+st.markdown("""
+Xin chào! Đây là ứng dụng dự báo chỉ số công tơ điện sử dụng mô hình học máy.
+
+👉 Sử dụng menu bên trái để chuyển qua trang dự đoán.
+""")
